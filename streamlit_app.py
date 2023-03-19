28afc0eff1822f9304bf907b08b974bfba762e68
Rajivvashistsnowflake <128268408+Rajivvashistsnowflake@users.noreply.github.com>
2023-03-19 12:57:57 +1100

    Update streamlit_app.py
    use of pandas to read csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 
-import streamlit
+import streamlit, pandas
 streamlit.title("My Parents new Healthy Diner at 207 oak street")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
