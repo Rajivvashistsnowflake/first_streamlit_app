37b6203425e25916514a7f35b33849aa5251038a
Rajivvashistsnowflake <128268408+Rajivvashistsnowflake@users.noreply.github.com>
2023-03-20 14:35:11 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,19 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+
+my_cur.execute(" select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("Fruit list contains:")
+streamlit.text(my_data_row)
+
+
+my_data_row = my_cur.fetchone()
+streamlit.header("Fruit list contains:")
+streamlit.dataframe(my_data_row)
+
+my_cur.execute(" select * from fruit_load_list")
+my_data_row = my_cur.fetchall()
+streamlit.header("Fruit list contains:")
+streamlit.dataframe(my_data_row)
